7a014f5dce92d58039e9e65ba58986e9b33676de
lexseven <q213546879@hotmail.com>
2019-04-09 10:52:55 +0800

    mac

--- example/store/js/doc.js
+++ example/store/js/doc.js
@@ -5,17 +5,17 @@ var data = {
             {
                 "authorId": 43,
                 "authorName": "GUCCI/古驰123",
-                "authorPic": "作者头像",
+                "authorPic": "https://pic12.secooimg.com/res/contentERP/98/48/1WvGkEb037027f973041698ac3808ed1f80d78.png",
                 "authorType": 2,
-                "commentCount": 17127,
+                "commentCount": 438,
                 "commentVideo": "测试内容j85s",
                 "commentVideoImg": "测试内容554d",
                 "contentType": 1,
-                "id": 53562,
+                "id": "晒单ID",
                 "identityLable": "测试内容5ed5",
                 "imagesJson": [
-                    "https://pic12.secooimg.com/res/contentERP/10/53/1WvJm4f5172d26d19c4019b93e01b5fac020e0.jpg",
-                    "https://pic12.secooimg.com/res/contentERP/98/48/1WvGkEb037027f973041698ac3808ed1f80d78.png"
+                    "https://hbimg.huabanimg.com/e3cd170c2cfc2cc850ac9d7e0cebe3e2642134c722eb55-uRwd2N_fw658",
+                    "https://hbimg.huabanimg.com/22bca12eaa4ac13afae3f4f083d9ecd63c7e704111952-nYdR0q_fw658"
                 ],
                 "optionTime": 1536139328211,
                 "optionType": 0,
@@ -23,10 +23,10 @@ var data = {
                 "relationBrand": "3",
                 "relationSign": "3",
                 "relationSku": "3",
-                "shareCount": 0,
+                "shareCount": 234,
                 "sourceType": 1,
-                "storeType": 1,
-                "thumbsCount": 0,
+                "storeType": 2,
+                "thumbsCount": 23,
                 "thumbsStatus": false,
                 "title": "3333",
                 "uuid": "2e9adeadbd9046219f46878886a3dac4"
@@ -34,7 +34,7 @@ var data = {
             {
                 "authorId": 43,
                 "authorName": "GUCCI/古驰1",
-                "authorPic": "作者头像",
+                "authorPic": "https://hbimg.huabanimg.com/120ecc95c488afc070fad93fd85cb05d47a0a2a033139-l13udx_/fw/480",
                 "authorType": 2,
                 "commentCount": 17127,
                 "commentVideo": "测试内容j85s",
@@ -43,8 +43,10 @@ var data = {
                 "id": 53562,
                 "identityLable": "测试内容5ed5",
                 "imagesJson": [
-                    "https://pic12.secooimg.com/res/contentERP/10/53/1WvJm4f5172d26d19c4019b93e01b5fac020e0.jpg",
-                    "https://pic12.secooimg.com/res/contentERP/98/48/1WvGkEb037027f973041698ac3808ed1f80d78.png"
+                    "https://hbimg.huabanimg.com/c934774e41733417925a0740e31470e29318593115851-G9beMd_fw658",
+                    "https://hbimg.huabanimg.com/08be74cc43a48adb531222c4d92ffa4b26c8126b2bd6e-T7XR4T_fw658",
+                    "https://hbimg.huabanimg.com/5b6885632f3e8d5098dc19cda76016aa9123b36513a10-aeXkvl_/fw/480",
+                    "https://hbimg.huabanimg.com/77c089cd77c332d3e1c95b0f042c21560a6b120b8b5b-Ky1wDp_/fw/480"
                 ],
                 "optionTime": 1536139328211,
                 "optionType": 0,
@@ -54,8 +56,8 @@ var data = {
                 "relationSku": "3",
                 "shareCount": 0,
                 "sourceType": 1,
-                "storeType": 1,
-                "thumbsCount": 0,
+                "storeType": 4,
+                "thumbsCount": 583,
                 "thumbsStatus": false,
                 "title": "3333",
                 "uuid": "2e9adeadbd9046219f46878886a3dac4"
@@ -63,17 +65,18 @@ var data = {
             {
                 "authorId": 43,
                 "authorName": "GUCCI/古驰55",
-                "authorPic": "作者头像",
+                "authorPic": "https://hbimg.huabanimg.com/8de93d411b8878f162ded5383fcf16b66c7ac68b1a26f-JRlWIT_/fw/480",
                 "authorType": 2,
                 "commentCount": 17127,
                 "commentVideo": "测试内容j85s",
                 "commentVideoImg": "测试内容554d",
                 "contentType": 1,
-                "id": 53562,
+                "id": 53562,//
                 "identityLable": "测试内容5ed5",
                 "imagesJson": [
-                    "https://pic12.secooimg.com/res/contentERP/10/53/1WvJm4f5172d26d19c4019b93e01b5fac020e0.jpg",
-                    "https://pic12.secooimg.com/res/contentERP/98/48/1WvGkEb037027f973041698ac3808ed1f80d78.png"
+                    "https://hbimg.huabanimg.com/5a67ad2623c956e282f00c114441e8c2ac908bad257d5-FaLxuL_fw658",
+                    "https://hbimg.huabanimg.com/b888b9ec762c49c199cd831f9126e967197d943f2372f-wPrxBP_fw658",
+                    "https://hbimg.huabanimg.com/e4c2703d3b71c440107340e8a5a568f0db573ba02f457-NCa8Nt_fw658"
                 ],
                 "optionTime": 1536139328211,
                 "optionType": 0,
@@ -84,7 +87,7 @@ var data = {
                 "shareCount": 0,
                 "sourceType": 1,
                 "storeType": 1,
-                "thumbsCount": 0,
+                "thumbsCount": 789,
                 "thumbsStatus": false,
                 "title": "3333",
                 "uuid": "2e9adeadbd9046219f46878886a3dac4"
