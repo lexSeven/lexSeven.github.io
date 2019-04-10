a5e2d5a2012401c2593074bbd6f776ec954cce5e
lexseven <q213546879@hotmail.com>
2019-04-10 17:19:17 +0800

    change

--- example/store/js/doc.js
+++ example/store/js/doc.js
@@ -5,7 +5,41 @@ var data = {
             {
                 "authorId": 43,
                 "authorName": "GUCCI/古驰123",
-                "authorPic": "https://pic12.secooimg.com/res/contentERP/98/48/1WvGkEb037027f973041698ac3808ed1f80d78.png",
+                "authorPic": "https://hbimg.huabanimg.com/e2ae2d32067abeb75fea16a345f814bba55aaab410d59-g58L1F_/fw/480",
+                "authorType": 2,
+                "commentCount": 438,
+                "commentVideo": "测试内容j85s",
+                "commentVideoImg": "测试内容554d",
+                "contentType": 1,
+                "id": "晒单ID",
+                "identityLable": "测试内容5ed5",
+                "imagesJson": [
+                    "https://hbimg.huabanimg.com/c934774e41733417925a0740e31470e29318593115851-G9beMd_fw658",
+                    "https://hbimg.huabanimg.com/08be74cc43a48adb531222c4d92ffa4b26c8126b2bd6e-T7XR4T_fw658",
+                    "https://hbimg.huabanimg.com/5b6885632f3e8d5098dc19cda76016aa9123b36513a10-aeXkvl_/fw/480",
+                    "https://hbimg.huabanimg.com/77c089cd77c332d3e1c95b0f042c21560a6b120b8b5b-Ky1wDp_/fw/480",
+                    "https://hbimg.huabanimg.com/e3cd170c2cfc2cc850ac9d7e0cebe3e2642134c722eb55-uRwd2N_fw658",
+                    "https://hbimg.huabanimg.com/22bca12eaa4ac13afae3f4f083d9ecd63c7e704111952-nYdR0q_fw658",
+                    "https://hbimg.huabanimg.com/08be74cc43a48adb531222c4d92ffa4b26c8126b2bd6e-T7XR4T_fw658"
+                ],
+                "optionTime": 1536139328211,
+                "optionType": 0,
+                "priorityDisplay": 0,
+                "relationBrand": "3",
+                "relationSign": "3",
+                "relationSku": "3",
+                "shareCount": 234,
+                "sourceType": 1,
+                "storeType": 2,
+                "thumbsCount": 46238,
+                "thumbsStatus": false,
+                "title": "该页面中间的内容流展示部分（顶部店招和底部TAB为公共部分，无需处理）。内容类型包含后台创建的图文和短视频，对应于发现频道的晒单类型（功能以发现频道最新的功能为准，例如即将上线的关联多商品等",
+                "uuid": "2e9adeadbd9046219f46878886a3dac4"
+            },
+            {
+                "authorId": 43,
+                "authorName": "GUCCI/古驰123",
+                "authorPic": "https://hbimg.huabanimg.com/e2ae2d32067abeb75fea16a345f814bba55aaab410d59-g58L1F_/fw/480",
                 "authorType": 2,
                 "commentCount": 438,
                 "commentVideo": "测试内容j85s",
@@ -34,7 +68,7 @@ var data = {
             {
                 "authorId": 43,
                 "authorName": "GUCCI/古驰123",
-                "authorPic": "https://pic12.secooimg.com/res/contentERP/98/48/1WvGkEb037027f973041698ac3808ed1f80d78.png",
+                "authorPic": "https://hbimg.huabanimg.com/17806eb02f6c8fc07ab68e16f4aeb8fbb4897d5ae14b-jCFHjP_/fw/480",
                 "authorType": 2,
                 "commentCount": 438,
                 "commentVideo": "测试内容j85s",
@@ -124,6 +158,39 @@ var data = {
     },
     "extParam": 1,
     "msg": "请求成功"
+}
+
+var newData = {
+    "authorId": 43,
+    "authorName": "GUCCI/古驰3",
+    "authorPic": "https://hbimg.huabanimg.com/aa79ebb92a35ebbbd7d5e41aef837a774b655c2db85de-NhHtMw_/fw/480",
+    "authorType": 2,
+    "commentCount": 17127,
+    "commentVideo": "测试内容j85s",
+    "commentVideoImg": "测试内容554d",
+    "contentType": 1,
+    "id": 53562,
+    "identityLable": "测试内容5ed5",
+    "imagesJson": [
+        "https://hbimg.huabanimg.com/623b3c299553c1f50aad4b571bec9aefff01ee026c1c1-lRMzjU_fw658",
+        "https://hbimg.huabanimg.com/c135efdc58b336ffab0075639b067470118dc1c044d94-WXcvH1_/fw/480",
+        "https://hbimg.huabanimg.com/4e391ed9424dab0443e089e90406ed582d44d91b7558f-GCZ8NS_fw658",
+        "https://hbimg.huabanimg.com/85c8a63c47d77b33243653160edefffe7e94030dfbab5-rynoOi_fw658",
+        "https://hbimg.huabanimg.com/0dda96e4f166b3d02cb2a6803440c47bba1409d4c57c1-PJDg70_fw658"
+    ],
+    "optionTime": 1536139328211,
+    "optionType": 0,
+    "priorityDisplay": 0,
+    "relationBrand": "3",
+    "relationSign": "3",
+    "relationSku": "3",
+    "shareCount": 0,
+    "sourceType": 1,
+    "storeType": 1,
+    "thumbsCount": 0,
+    "thumbsStatus": false,
+    "title": "3333",
+    "uuid": "2e9adeadbd9046219f46878886a3dac4"
 }
 
 /*
