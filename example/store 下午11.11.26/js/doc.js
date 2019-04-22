f0b67d7a7137ed45c18d88e0a2cc09c577c76f98
lexseven <q213546879@hotmail.com>
2019-04-22 21:31:12 +0800

    change

---
+++ example/store 下午11.11.26/js/doc.js
@@ -0,0 +1,199 @@
+var data = {
+    "code": 10000,
+    "data": {
+        "list": [
+            {
+                "authorId": 43,
+                "authorName": "GUCCI/古驰123",
+                "authorPic": "https://hbimg.huabanimg.com/e2ae2d32067abeb75fea16a345f814bba55aaab410d59-g58L1F_/fw/480",
+                "authorType": 2,
+                "commentCount": 438,
+                "commentVideo": "测试内容j85s",
+                "commentVideoImg": "测试内容554d",
+                "contentType": 1,
+                "id": "晒单ID",
+                "identityLable": "测试内容5ed5",
+                "imagesJson": [
+                    "https://hbimg.huabanimg.com/c934774e41733417925a0740e31470e29318593115851-G9beMd_fw658",
+                    "https://hbimg.huabanimg.com/08be74cc43a48adb531222c4d92ffa4b26c8126b2bd6e-T7XR4T_fw658",
+                    "https://hbimg.huabanimg.com/5b6885632f3e8d5098dc19cda76016aa9123b36513a10-aeXkvl_/fw/480",
+                    "https://hbimg.huabanimg.com/77c089cd77c332d3e1c95b0f042c21560a6b120b8b5b-Ky1wDp_/fw/480",
+                    "https://hbimg.huabanimg.com/e3cd170c2cfc2cc850ac9d7e0cebe3e2642134c722eb55-uRwd2N_fw658",
+                    "https://hbimg.huabanimg.com/22bca12eaa4ac13afae3f4f083d9ecd63c7e704111952-nYdR0q_fw658",
+                    "https://hbimg.huabanimg.com/08be74cc43a48adb531222c4d92ffa4b26c8126b2bd6e-T7XR4T_fw658"
+                ],
+                "optionTime": 1536139328211,
+                "optionType": 0,
+                "priorityDisplay": 0,
+                "relationBrand": "3",
+                "relationSign": "3",
+                "relationSku": "3",
+                "shareCount": 234,
+                "sourceType": 1,
+                "storeType": 2,
+                "thumbsCount": 46238,
+                "thumbsStatus": false,
+                "title": "该页面中间的内容流展示部分（顶部店招和底部TAB为公共部分，无需处理）。内容类型包含后台创建的图文和短视频，对应于发现频道的晒单类型（功能以发现频道最新的功能为准，例如即将上线的关联多商品等",
+                "uuid": "2e9adeadbd9046219f46878886a3dac4"
+            },
+            {
+                "authorId": 43,
+                "authorName": "GUCCI/古驰123",
+                "authorPic": "https://hbimg.huabanimg.com/e2ae2d32067abeb75fea16a345f814bba55aaab410d59-g58L1F_/fw/480",
+                "authorType": 2,
+                "commentCount": 438,
+                "commentVideo": "测试内容j85s",
+                "commentVideoImg": "测试内容554d",
+                "contentType": 1,
+                "id": "晒单ID",
+                "identityLable": "测试内容5ed5",
+                "imagesJson": [
+                    "https://hbimg.huabanimg.com/e3cd170c2cfc2cc850ac9d7e0cebe3e2642134c722eb55-uRwd2N_fw658",
+                    "https://hbimg.huabanimg.com/22bca12eaa4ac13afae3f4f083d9ecd63c7e704111952-nYdR0q_fw658"
+                ],
+                "optionTime": 1536139328211,
+                "optionType": 0,
+                "priorityDisplay": 0,
+                "relationBrand": "3",
+                "relationSign": "3",
+                "relationSku": "3",
+                "shareCount": 234,
+                "sourceType": 1,
+                "storeType": 2,
+                "thumbsCount": 23,
+                "thumbsStatus": false,
+                "title": "3333",
+                "uuid": "2e9adeadbd9046219f46878886a3dac4"
+            },
+            {
+                "authorId": 43,
+                "authorName": "GUCCI/古驰123",
+                "authorPic": "https://hbimg.huabanimg.com/17806eb02f6c8fc07ab68e16f4aeb8fbb4897d5ae14b-jCFHjP_/fw/480",
+                "authorType": 2,
+                "commentCount": 438,
+                "commentVideo": "测试内容j85s",
+                "commentVideoImg": "测试内容554d",
+                "contentType": 1,
+                "id": "晒单ID",
+                "identityLable": "测试内容5ed5",
+                "imagesJson": [
+                    "https://hbimg.huabanimg.com/e3cd170c2cfc2cc850ac9d7e0cebe3e2642134c722eb55-uRwd2N_fw658"
+                ],
+                "optionTime": 1536139328211,
+                "optionType": 0,
+                "priorityDisplay": 0,
+                "relationBrand": "3",
+                "relationSign": "3",
+                "relationSku": "3",
+                "shareCount": 234,
+                "sourceType": 1,
+                "storeType": 2,
+                "thumbsCount": 23,
+                "thumbsStatus": false,
+                "title": "3333",
+                "uuid": "2e9adeadbd9046219f46878886a3dac4"
+            },
+            {
+                "authorId": 43,
+                "authorName": "GUCCI/古驰1",
+                "authorPic": "https://hbimg.huabanimg.com/120ecc95c488afc070fad93fd85cb05d47a0a2a033139-l13udx_/fw/480",
+                "authorType": 2,
+                "commentCount": 17127,
+                "commentVideo": "测试内容j85s",
+                "commentVideoImg": "测试内容554d",
+                "contentType": 1,
+                "id": 53562,
+                "identityLable": "测试内容5ed5",
+                "imagesJson": [
+                    "https://hbimg.huabanimg.com/c934774e41733417925a0740e31470e29318593115851-G9beMd_fw658",
+                    "https://hbimg.huabanimg.com/08be74cc43a48adb531222c4d92ffa4b26c8126b2bd6e-T7XR4T_fw658",
+                    "https://hbimg.huabanimg.com/5b6885632f3e8d5098dc19cda76016aa9123b36513a10-aeXkvl_/fw/480",
+                    "https://hbimg.huabanimg.com/77c089cd77c332d3e1c95b0f042c21560a6b120b8b5b-Ky1wDp_/fw/480"
+                ],
+                "optionTime": 1536139328211,
+                "optionType": 0,
+                "priorityDisplay": 0,
+                "relationBrand": "3",
+                "relationSign": "3",
+                "relationSku": "3",
+                "shareCount": 0,
+                "sourceType": 1,
+                "storeType": 4,
+                "thumbsCount": 583,
+                "thumbsStatus": false,
+                "title": "3333",
+                "uuid": "2e9adeadbd9046219f46878886a3dac4"
+            },
+            {
+                "authorId": 43,
+                "authorName": "GUCCI/古驰55",
+                "authorPic": "https://hbimg.huabanimg.com/8de93d411b8878f162ded5383fcf16b66c7ac68b1a26f-JRlWIT_/fw/480",
+                "authorType": 2,
+                "commentCount": 17127,
+                "commentVideo": "测试内容j85s",
+                "commentVideoImg": "测试内容554d",
+                "contentType": 1,
+                "id": 53562,//
+                "identityLable": "测试内容5ed5",
+                "imagesJson": [
+                    "https://hbimg.huabanimg.com/5a67ad2623c956e282f00c114441e8c2ac908bad257d5-FaLxuL_fw658",
+                    "https://hbimg.huabanimg.com/b888b9ec762c49c199cd831f9126e967197d943f2372f-wPrxBP_fw658",
+                    "https://hbimg.huabanimg.com/e4c2703d3b71c440107340e8a5a568f0db573ba02f457-NCa8Nt_fw658"
+                ],
+                "optionTime": 1536139328211,
+                "optionType": 0,
+                "priorityDisplay": 0,
+                "relationBrand": "3",
+                "relationSign": "3",
+                "relationSku": "3",
+                "shareCount": 0,
+                "sourceType": 1,
+                "storeType": 1,
+                "thumbsCount": 789,
+                "thumbsStatus": false,
+                "title": "3333",
+                "uuid": "2e9adeadbd9046219f46878886a3dac4"
+            }
+        ]
+    },
+    "extParam": 1,
+    "msg": "请求成功"
+}
+
+var newData = {
+    "authorId": 43,
+    "authorName": "GUCCI/古驰3",
+    "authorPic": "https://hbimg.huabanimg.com/aa79ebb92a35ebbbd7d5e41aef837a774b655c2db85de-NhHtMw_/fw/480",
+    "authorType": 2,
+    "commentCount": 17127,
+    "commentVideo": "测试内容j85s",
+    "commentVideoImg": "测试内容554d",
+    "contentType": 1,
+    "id": 53562,
+    "identityLable": "测试内容5ed5",
+    "imagesJson": [
+        "https://hbimg.huabanimg.com/623b3c299553c1f50aad4b571bec9aefff01ee026c1c1-lRMzjU_fw658",
+        "https://hbimg.huabanimg.com/c135efdc58b336ffab0075639b067470118dc1c044d94-WXcvH1_/fw/480",
+        "https://hbimg.huabanimg.com/4e391ed9424dab0443e089e90406ed582d44d91b7558f-GCZ8NS_fw658",
+        "https://hbimg.huabanimg.com/85c8a63c47d77b33243653160edefffe7e94030dfbab5-rynoOi_fw658",
+        "https://hbimg.huabanimg.com/0dda96e4f166b3d02cb2a6803440c47bba1409d4c57c1-PJDg70_fw658"
+    ],
+    "optionTime": 1536139328211,
+    "optionType": 0,
+    "priorityDisplay": 0,
+    "relationBrand": "3",
+    "relationSign": "3",
+    "relationSku": "3",
+    "shareCount": 0,
+    "sourceType": 1,
+    "storeType": 1,
+    "thumbsCount": 0,
+    "thumbsStatus": false,
+    "title": "3333",
+    "uuid": "2e9adeadbd9046219f46878886a3dac4"
+}
+
+/*
+http://apidoc.siku.cn/workspace/myWorkspace.do?projectId=264#1553
+点赞 分享接口文档
+ */
