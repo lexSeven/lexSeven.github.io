3731d56f0a46be378630c7532a4c3aae0d917ca6
lexseven <q213546879@hotmail.com>
2019-05-27 14:03:35 +0800

    change

--- src/pages/deskTop/App.vue
+++ src/pages/deskTop/App.vue
@@ -1,86 +1,24 @@
 <template>
     <div id="app" class="wrap">
-        <stepOne
-            :parentdata="invitationData"
-            v-if="showItem===1"
-            :changeShow = changeShow
-        ></stepOne>
-        <stepTwo
-            :parentdata="invitationData"
-            :changeShow = changeShow
-            v-if="showItem===2"
-        ></stepTwo>
-        <attestation
-            v-if="showItem===3"
-            :changeShow = changeShow
-        ></attestation>
+        <BottomLine></BottomLine>
     </div>
 </template>
 
 <script>
-
-// 发布邀约第二步
-import stepTwo from './components/stepTwo.vue'
+import BottomLine from './components/BottomLine'
 export default {
     name: 'App',
     data () {
         return {
             cityName: '北京',
-            invitationData: {
-                accurateAdd: '', // 地点经纬度
-                addressName: '', // 活动点名称
-                area: '', // 邀约所在区
-                areaId: '', // 区ID
-                city: '', // 邀约所在城市
-                cityId: '', // 城市ID
-                province: '', // 邀约所在省
-                provinceId: '', // 省ID
-                startTime: '', // 邀约开始时间
-                endTime: '', // 邀约结束时间
-                image: '', // 展示图
-                invitationName: '', // 邀约名称
-                invitationRange: '', // 邀约范围
-                invitationTypeId: '', // 邀约类型ID
-                number: '', // 参与人数
-                remark: '', // 邀约描述
-                shopId: '', // 店铺ID
-                upk: '', // 用户登录标识
-                weChat: '' // 邀约发起人微信
-            },
-            showItem: 1
         }
     },
-    methods: {
-        changeCityName: function (ev) {
-            this.cityName = ev.currentTarget.innerText
-        },
-        changeData: function (option) {
-            Object.assign(this.invitationData, option)
-        },
-        changeShow: function (num) {
-            this.showItem = Number(num)
-            localStorage.setItem('nowPage', this.showItem)
-        },
-        handleSearch: function () {
-            let q = {}
-            location.search.replace(/([^?&=]+)=([^&]+)/g,(_,k,v)=>q[k]=v)
-            return q
-        },
-        changeToattes: function (boolean) {
-            this.attStatus = boolean
-        }
-    },
+    methods: {},
     computed: {},
     updated () {},
-    mounted () {
-        console.log('app')
-        console.log(localStorage.getItem('nowPage'))
-        if (localStorage.getItem('nowPage')) {
-            this.showItem = Number(localStorage.getItem('nowPage'))
-        }
-    },
+    mounted () {},
     components: {
-        stepTwo,
+        BottomLine
     }
 }
 </script>
@@ -88,5 +26,5 @@ export default {
 <style lang="scss" scoped>
 @import "~assets/style/comment.css";
 @import "~assets/style/qqMusic.css";
-@import "~assets/style/bottomLine.css";
+
 </style>
---
+++ src/pages/deskTop/components/BottomLine.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="stepTwo wrap">
+
+    </div>
+</template>
+<script>
+import api from 'services/index'
+import { concatUrl } from 'common/utils'
+export default {
+    name: 'about',
+    data () {
+        return {
+            name: '底部菜单栏',
+        }
+    },
+    props: {},
+    watch: {},
+    methods: {},
+    created: function () {},
+    mounted: function () {}
+}
+</script>
+<style>
+@import "~assets/style/bottomLine.css";
+</style>
